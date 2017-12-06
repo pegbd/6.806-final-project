71d891be2f3e398a9ab143ce239b57782eb0ec7b
Patrick Ikedi Egbuchulam <patrickikediegbuchulam@Patricks-MacBook-Pro.local>
2017-12-06 00:56:13 -0500

    Trying to get the model to train, but running into a variable/tensor error

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -9,6 +9,8 @@ Takes in data from ../askubuntu-master/vector/vectors_pruned.200.txt
 """
 
 # wrappers for data types
+def to_int_variable(data):
+    return Variable(torch.IntTensor(data))
 
 def to_float_variable(data):
     return Variable(torch.FloatTensor(data))
@@ -16,36 +18,65 @@ def to_float_variable(data):
 def to_long_variable(data):
     return Variable(torch.LongTensor(data))
 
+def split_into_batches(data, batch_size):
+    batches = []
+    div = int(len(data) / batch_size)
+    rem = len(data) % batch_size
+
+    for i in xrange(0, len(data) - rem, batch_size):
+        batch = data[i:i+batch_size]
+        batches.append(batch)
+
+    batches[-1] = batches[-1] + data[len(data) - batch_size + 1:]
+
+    return batches
+
 
 # create the word_to_vector dictionary
 
-f = open('./askubuntu-master/vector/vectors_pruned.200.txt', 'r')
+f = open('../askubuntu-master/vector/vectors_pruned.200.txt', 'r')
 
 word_to_vector = {}
 for line in f.readlines():
     split = line.strip().split(" ")
     word = split[0]
 
-    vec = np.array([float(val) for val in split[1:]])
+    vec = [float(val) for val in split[1:]]
     word_to_vector[word] = vec
 
 f.close()
 
 # create the question dictionary
-f = open('./askubuntu-master/text_tokenized.txt', 'r')
+f = open('../askubuntu-master/text_tokenized.txt', 'r')
 
+vocab = set(word_to_vector.keys())
 id_to_question = {}
 for line in f.readlines():
     split = line.strip().split("\t")
 
     id_num = split[0].strip()
     title = split[1].strip()
-    body = split[2].strip()
+    body = split[2].strip() if len(split) == 3 else None
 
     # convert title and body to array of word vectors
-    title_matrix = [word_to_vector[w] for w in title.split()]
-    body_matrix = [word_to_vector[w] for w in body.split]
+    title_matrix = [word_to_vector[w] for w in title.split() if w in vocab]
+    body_matrix = [word_to_vector[w] for w in body.split() if w in vocab] if body is not None else []
 
-    id_to_question[id_num] = (np.array(title_matrix), np.array(body_matrix))
+    id_to_question[id_num] = (to_float_variable(title_matrix), to_float_variable(body_matrix))
+
+f.close()
+
+# create question -> candidates dictionary
+f = open('../askubuntu-master/train_random.txt', 'r')
+
+question_to_candidates = {}
+for line in f.readlines():
+    split = line.strip().split('\t')
+
+    question = split[0]
+    positive = split[1].split(' ')
+    negative = split[2].split(' ')
+
+    question_to_candidates[question] = (positive, negative)
 
 f.close()
--- code/qr_lstm.py
+++ code/qr_lstm.py
@@ -5,20 +5,29 @@ from torch.nn import functional as F
 import numpy as np
 import preprocessing
 
-# implementation 1, built in torch LSTM
+####################################################################
+########################## Custom Classes ##########################
+####################################################################
 
-# lstm = torch.nn.LSTM()
+class QR_Maximum_Margin_Loss(torch.autograd.Function):
 
-# implementation 2, custom model
+    def forward(self, q, positive, negatives):
+        del_value = .01
+        s_positive = F.cosine_similarity(q, positive, 0)
+        return torch.max(torch.stack([F.cosine_similarity(q, n, 0) - s_positive + del_value for n in negatives]))
+
 
 class LSTM_Cell(nn.Module):
     """
     An individual LSTM cell
     """
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super(LSTM, self).__init__()
+    def __init__(self, input_dim, hidden_dim, output_dim, use_bias=False):
+        super(LSTM_Cell, self).__init__()
 
         self.use_bias = use_bias # for later, perhaps?
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
 
         # input weights
         self.W_i = nn.Linear(input_dim, hidden_dim)
@@ -35,53 +44,152 @@ class LSTM_Cell(nn.Module):
         # output/decoder
         self.h2o = nn.Linear(hidden_dim, output_dim)
 
-        def forward(self, x_t, h_t_1, c_t_1):
+    def forward_step(self, x_t, h_t_1, c_t_1):
 
-            """
-            inputs:
+        """
+        Computes one forward step (within a larger sequence)
 
-            x_t = word t in the sentence
-            h_t_1 = the hidden state at step t - 1
-            c_t_1 = the visible state at step t - 1
+        inputs:
 
-            outputs:
+        x_t = word t in the sentence
+        h_t_1 = the hidden state at step t - 1
+        c_t_1 = the visible state at step t - 1
 
-            h_t = the current hidden state
-            c_t = the current visible state
+        outputs:
 
-            """
+        h_t = the current hidden state
+        c_t = the current visible state
 
-            # input gate
-            i = F.sigmoid(self.W_i(x_t) + self.U_i(h_t_1))
+        """
 
-            # forget gate
-            f = F.sigmoid(self.W_f(x_t) + self.U_f(h_t_1))
+        # input gate
+        i = F.sigmoid(self.W_i(x_t) + self.U_i(h_t_1))
 
-            # output gate
-            o = F.sigmoid(self.W_o(x_t) + self.U_o(h_t_1))
+        # forget gate
+        f = F.sigmoid(self.W_f(x_t) + self.U_f(h_t_1))
 
-            # activation
-            z = F.tanh(self.W_z(x_t) + self.U_z(h_t_1))
+        # output gate
+        o = F.sigmoid(self.W_o(x_t) + self.U_o(h_t_1))
 
-            # calculate the next visible asnd hidden states, then return
-            c_t = (i * z) + (f * c_t_1)
-            h_t = o * F.tanh(c_t)
+        # activation
+        z = F.tanh(self.W_z(x_t) + self.U_z(h_t_1))
 
-            return h_t, c_t
+        # calculate the next visible asnd hidden states, then return
+        c_t = (i * z) + (f * c_t_1)
+        h_t = o * F.tanh(c_t)
 
-# Work Space
-words = preprocessing.word_to_vector
-questions = preprocessing.id_to_question
+        return h_t, c_t
+
+    def forward(self, X):
+
+        """
+        Computes forward_step over the entire sequence X, and returns the averages of
+        the hidden and visible states
+
+        inputs:
+
+        X = entire matrix sequence of word vectors
+
+        outputs:
+
+        h_avg = the average over the entire sequence of hiddens states
+        c_avg = the average over the entire sequence of visible states
+        """
+
+        # initialize hidden and visible states at t = 0
+        h_0 = preprocessing.to_float_variable([0.0 for i in range(self.hidden_dim)])
+        c_0 = preprocessing.to_float_variable([0.0 for i in range(self.hidden_dim)])
+
+        h = h_0
+        c = c_0
+
+        h_prev = h_0
+        c_prev = c_0
+
+        for i in range(len(X)):
+            h_t, c_t = self.forward_step(X[i], h_prev, c_prev)
+
+            h += h_t
+            c += c_t
+            h_prev = h_t
+            c_prev = c_t
+
+        h_avg = h/len(X)
+        c_avg = c/len(X)
+
+        return (h_avg, c_avg)
+
+####################################################################
+############################# TRAINING #############################
+####################################################################
 
 # Network Setup
 torch.manual_seed(1)
-INPUT_SIZE = 300
+INPUT_SIZE = 200
 HIDDEN_SIZES = [INPUT_SIZE/4, INPUT_SIZE/2, INPUT_SIZE, 2*INPUT_SIZE]
 OUTPUT_SIZE = 2
 LEARNING_RATES = [.00001, .001, .1, 10]
 L2_NORMS = [.00001, .001, 10]
-NUM_ITERATIONS = 50
+NUM_ITERATIONS = 1
+BATCH_SIZE = 64
+
+# Work Space
+words = preprocessing.word_to_vector
+questions = preprocessing.id_to_question
+candidates = preprocessing.question_to_candidates
+
+training_batches = preprocessing.split_into_batches(candidates.keys(), BATCH_SIZE)
 
 # Initialize Network and Optimizer
-lstm = LSTM_Cell(INPUT_SIZE, HIDDEN_SIZES[0], OUTPUT_SIZE)
+lstm = LSTM_Cell(input_dim=INPUT_SIZE, hidden_dim=HIDDEN_SIZES[1], output_dim=OUTPUT_SIZE)
 optimizer = optim.Adam(params=lstm.parameters(), lr=LEARNING_RATES[0], weight_decay=L2_NORMS[0])
+
+for batch in training_batches:
+    for i in range(NUM_ITERATIONS):
+        batch_loss = 0
+        for qr in batch:
+
+            # get the title and body matrices for the question, the positive and negative candidates
+            q = questions[qr]
+            pos = [questions[p] for p in candidates[qr][0]]
+            neg = [questions[n] for n in candidates[qr][1]]
+
+            # model representation of question q
+            q_title_avg, _ = lstm(q[0])
+            q_body_avg, _ = lstm(q[1])
+            q_avg = (q_title_avg + q_body_avg) / 2
+
+            # model representation of positive candidates
+            pos_avgs = []
+            for pos_cand in pos:
+                p_title_avg, _ = lstm(pos_cand[0])
+                p_body_avg, _ = lstm(pos_cand[1])
+                p_avg = (p_title_avg + p_body_avg) / 2
+
+                pos_avgs.append(p_avg)
+
+            # model representation of negative candidates
+            neg_avgs = []
+            for neg_cand in neg:
+                n_title_avg, _ = lstm(neg_cand[0])
+                n_body_avg, _ = lstm(neg_cand[1])
+                n_avg = (n_title_avg + n_body_avg) / 2
+
+                neg_avgs.append(n_avg)
+
+            mml = QR_Maximum_Margin_Loss()
+
+            negatives = torch.stack(neg_avgs)
+            loss = mml(q_avg, pos_avgs[0], negatives)
+            print("loss ", loss)
+            batch_loss += loss
+
+            # back propogate with gradients
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print ("batch loss = ", batch_loss)
+        print ("------------------------")
+
+lstm.save_state_dict('mytraininglstm.pt')
